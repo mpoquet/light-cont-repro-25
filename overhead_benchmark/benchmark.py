e661253f440bce330858304a6dab46b1e1b166d9
aymericfrcs <aymeric.frcs@gmail.com>
2025-04-26 22:09:22 +0200

    Ajout test natif à benchmark.py

--- overhead_benchmark/benchmark.py
+++ overhead_benchmark/benchmark.py
@@ -6,12 +6,12 @@ N = 30
 FILE_OUT: str = "./data.csv"
 IMAGE: str = "./docker-image-sysbench.tar.gz"
 IMAGE_NAME = "sysbench:tp"
-ENGINES: list[str] = ["ctr", "podman", "docker"] #, "crictl"]
+ENGINES: list[str] = ["native", "ctr", "podman", "docker"] #, "crictl"]
 # TODO ajouter CRI-O
 
 SYSBENCH_ARGS: dict[str, list[str]] = {
-        "fileio": ["--test=fileio", "--file-test-mode=seqwr", "--file-num=1"],
-        "cpu": ["--test=cpu", "--cpu-max-prime=2000"],
+        "fileio": ["fileio", "--file-test-mode=seqwr", "--file-num=1"],
+        "cpu": ["cpu", "--cpu-max-prime=2000"],
         }
 
 def print_running_cmd(cmd: list[str]):
@@ -34,6 +34,8 @@ def generate_run_cmd(engine: str, sysbench_args: list[str]) -> list[str]:
         return [engine, "run", "--rm", "docker.io/library/"+IMAGE_NAME, "sysbench", "sysbench"] + sysbench_args + ["run"]
     if engine == "crictl":
         return [engine, "exec", IMAGE_NAME, "sysbench"] + sysbench_args + ["run"]
+    if engine == "native":
+        return ["sysbench"] + sysbench_args + ["run"]
     return None
 
 
@@ -41,11 +43,12 @@ def main():
 
 
     for engine in ENGINES:
-        print("Loading image for {}...".format(engine))
-        load_cmd = generate_load_cmd(engine)
-        print_running_cmd(load_cmd)
-        run(load_cmd, stdout = DEVNULL)
-    
+        if engine != "native":
+            print("Loading image for {}...".format(engine))
+            load_cmd = generate_load_cmd(engine)
+            print_running_cmd(load_cmd)
+            run(load_cmd, stdout = DEVNULL)
+        
     print("Begin tests")
     for engine in ENGINES:
         for mode in SYSBENCH_ARGS:
@@ -54,10 +57,13 @@ def main():
             for n in range(1, N + 1):
                 print("[engine={}, test={}, num={}/{}]".format(engine, mode, n, N))
                 #print_running_cmd(run_cmd)
-                p1 = Popen(run_cmd, stdout = PIPE, stderr = DEVNULL)
+                p1 = Popen(run_cmd, stdout=PIPE, stderr=DEVNULL)
                 parse_cmd = ["python", "./sysbench_script.py", "-c", engine, "-b", mode, "-o", FILE_OUT]
-                p2 = Popen(parse_cmd, stdin = p1.stdout, stdout = PIPE)
-                p1.communicate(p2)
+                p2 = Popen(parse_cmd, stdin=p1.stdout, stdout=PIPE)
+                p1.stdout.close()   # Très important
+                p2.communicate()
+                p1.wait()
+
 
 if __name__ == "__main__":
     main()
