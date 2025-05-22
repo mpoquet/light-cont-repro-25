2f7aa967092291173202e3f99adedadefda2ba59
aymericfrcs <aymeric.frcs@gmail.com>
2025-05-22 03:11:29 +0200

    Benchmark works for docker, podman, light-cont, and almost for native
    Désactivé fileio pour ramfs pour l'instant. Marche nickel pour podman, docker et light-cont. Pour native, le process ne trouve pas ./rootfs/bin/sysbench alors que le fichier est bien là - donc remplacé par sysbench de l'hôte (même version) pour l'instant. Commencé support pour crun et youki, manque à générer plusieurs config.json à interchanger suivant le benchmark (besoin de changer la commande dans le champ args de config). Limité à 2 iter par bench par engine et à 10 sec d'exec pour tests.

--- overhead_benchmark/benchmark.py
+++ overhead_benchmark/benchmark.py
@@ -1,17 +1,45 @@
 from subprocess import *
 import os
 
-N = 30
+N = 2
 
 FILE_OUT: str = "./data.csv"
 IMAGE: str = "./docker-image-sysbench.tar.gz"
 IMAGE_OCI: str = "./sysbench.oci.tar"
 IMAGE_OCI_PATH_FROM_MR: str = "../overhead_benchmark/sysbench.oci.tar"
 IMAGE_NAME = "sysbench:tp"
-ENGINES: list[str] = ["light-cont", "native", "ctr", "podman", "docker"]
+ENGINES: list[str] = ["light-cont", "native", "podman", "docker"] #crun, youki, styrolite, runc
 
 SYSBENCH_ARGS: dict[str, list[str]] = {
+    "cpu": [
+        "cpu",
+        "--cpu-max-prime=2000",  # More demanding workload
+        "--threads=4",             # Multi-threaded
+        "--time=10"                # Fixed duration
+    ],
+
     "fileio": [
+        "fileio",
+        "--file-test-mode=seqwr",
+        "--file-num=1",
+        "--time=10"
+    ],
+
+    "memory": [
+        "memory",
+        "--memory-block-size=4K",   # Common page size
+        "--memory-total-size=20G",  # Large enough to exceed cache
+        "--memory-oper=write",      # Or mixed with --memory-access-mode=rnd
+        "--memory-scope=global",    # Test full system memory bandwidth
+        "--threads=4",              # Multi-threaded
+        "--time=10"
+    ]
+    
+}
+
+'''
+#args pour tester avec ramfs
+"fileio": [
         "fileio",
         "--file-test-mode=rndrw",
         "--file-total-size=2G",
@@ -19,23 +47,11 @@ SYSBENCH_ARGS: dict[str, list[str]] = {
         "--file-extra-flags=direct", # Bypass cache
         "--file-fsync-freq=100",
         "--file-rw-ratio=4",
-        "--file-path=/mnt/ramfs"  # Pointing to RAMFS
-    ],
-    "cpu": [
-        "cpu",
-        "--cpu-max-prime=100000",  # More demanding workload
-        "--threads=4",             # Multi-threaded
-        "--time=30"                # Fixed duration
-    ],
-    "memory": [
-        "memory",
-        "--memory-block-size=4K",   # Common page size
-        "--memory-total-size=20G",  # Large enough to exceed cache
-        "--memory-oper=write",      # Or mixed with --memory-access-mode=rnd
-        "--memory-scope=global",    # Test full system memory bandwidth
-        "--threads=4"              # Multi-threaded
+        "--file-path=/mnt/ramfs",  # Pointing to RAMFS
+        "--time=10"
     ]
-}
+'''
+
 
 def setup_ramfs():
     ramfs_path = "/mnt/ramfs"
@@ -57,8 +73,7 @@ def generate_load_cmd(engine: str) -> list[str]:
     if engine == "crictl":
         return [engine, "load", IMAGE]
     if engine == "light-cont":
-        return ["../minimalist_runtime/light-cont", "--path", "../overhead_benchmark/sysbench.oci.tar", "--extractpath ./sysbench-rootfs", "--run \"nothing\""]
-        #ne fonctionne pas pour l'ins
+        return ["../minimalist_runtime/light-cont", "--path", "./sysbench.oci.tar", "--extractpath", "./sysbench-rootfs", "--run", "\"nothing\""]
         #la commande nothing ne va pas être trouvé dans le fs donc light-cont va extraire l'image dans sysbench-rootfs et se terminer en renvoyant une erreur
         #pas encore implémenté le fait de pouvoir extract une image sans rien lancer dessus
     return None
@@ -71,11 +86,21 @@ def generate_run_cmd(engine: str, sysbench_args: list[str]) -> list[str]:
     if engine == "crictl":
         return [engine, "exec", IMAGE_NAME, "sysbench"] + sysbench_args + ["run"]
     if engine == "native":
-        return ["sysbench"] + sysbench_args + ["run"]
+        return ["sysbench"] + sysbench_args + ["run"] # ne trouve pas ./rootfs/bin/sysbench alors que le fichier existe
     if engine == "light-cont":
-        sysbench_cmd = ["\"/bin/sysbench"] + sysbench_args + ["\"" ]
-        print("sysbench_cmd = {}".format(sysbench_cmd))
-        return ["../minimalist_runtime/light-cont", "--extracted", "--path ./sysbench-rootfs", "--run "] + sysbench_cmd + ["run"]
+        full_cmd = ["/bin/sysbench"] + sysbench_args + ["run"]
+        cmd_str = " ".join(full_cmd)  # Ceci donne UNE chaîne
+        return [
+            "../minimalist_runtime/light-cont",
+            "--extracted",
+            "--path", "./sysbench-rootfs",
+            "--run", cmd_str
+        ]
+    if engine in ["crun", "youki"]:
+        return [engine, "run", "--bundle", ".", "sysbench"]
+    if engine == "styrolite":
+        #todo
+        return None
     return None
 
 def cleanup():
@@ -83,13 +108,15 @@ def cleanup():
     os.rmdir("/mnt/ramfs")
 
 def main():
+    '''
     # Configurer RAMFS pour les tests fileio
-    if any("fileio" in args for args in SYSBENCH_ARGS.values()):
+    if any("fileioramfs" in args for args in SYSBENCH_ARGS.values()):
         print("Setting up RAMFS for fileio tests...")
         setup_ramfs()
+    '''
 
     for engine in ENGINES:
-        if engine != "native" and engine != "light-cont":
+        if engine != "native":
             print("Loading image for {}...".format(engine))
             load_cmd = generate_load_cmd(engine)
             print_running_cmd(load_cmd)
@@ -98,19 +125,21 @@ def main():
     print("Begin tests")
     for engine in ENGINES:
         for mode in SYSBENCH_ARGS:
+            #pour crun et youki:
+            #changer config.json pour avoir le fichier config avec la bonne commande à lancer
             sysbench_args: list[str] = SYSBENCH_ARGS[mode]
             run_cmd = generate_run_cmd(engine, sysbench_args)
             #print("DEBUG: run_cmd = ", run_cmd)
             for n in range(1, N + 1):
                 print("[engine={}, test={}, num={}/{}]".format(engine, mode, n, N))
                 #print_running_cmd(run_cmd)
-                p1 = Popen(run_cmd, stdout=PIPE, stderr=PIPE)
+                p1 = Popen(run_cmd, stdout=PIPE, stderr=PIPE) #stdout=PIPE
                 parse_cmd = ["python", "./sysbench_script.py", "-c", engine, "-b", mode, "-o", FILE_OUT]
                 p2 = Popen(parse_cmd, stdin=p1.stdout, stdout=PIPE)
-                p1.stdout.close()   # Très important
+                p1.stdout.close()   
                 p2.communicate()
                 p1.wait()
 
 if __name__ == "__main__":
     main()
-    cleanup()
+    #cleanup()
