123e8aac3e4ef479cba88ebaa0f3e72e023196d6
aymericfrcs <aymeric.frcs@gmail.com>
2025-06-02 00:05:43 +0200

    cleanup final

--- minimalist_runtime/pull_ubuntufs.sh
+++
@@ -1,7 +0,0 @@
-#mkdir -m 0777 /tmp/light-cont
-mkdir -m 0777 ./rootfs
-#cd /tmp/light-cont
-podman pull ubuntu
-container_id=$(podman run -d ubuntu:latest sleep 3600)
-podman export -o ubuntu_fs.tar $container_id
-podman rm -f $container_id
